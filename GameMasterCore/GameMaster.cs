67b09a4d9141f5dcbcef4e8e82e3e857a51b8d26
Ewa Zielińska <zielinskae@student.mini.pw.edu.pl>
2018-03-23 00:40:30 +0100

    "PerformDiscover" method

--- GameMasterCore/GameMaster.cs
+++ GameMasterCore/GameMaster.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Shared;
 using Shared.Components.Boards;
+using Shared.Components.Fields;
+using Shared.Components.Players;
 using Shared.Messages.Communication;
 
 namespace GameMasterCore
@@ -22,10 +24,58 @@ namespace GameMasterCore
 
         public Data PerformDiscover(Discover discoverRequest)
         {
-            //znajdź playera po id w request
+            //Find player and its on-board representation (aka pawn)
             Player player = GetPlayerForGuid(discoverRequest.playerGuid);
-            //zrób discover
-            throw new NotImplementedException();
+            IPlayer playerPawn = board.GetPlayer(player.id);
+            //Prepare result partial structures
+            List<TaskField> resultFields = new List<TaskField>();
+            List<Piece> resultPieces = new List<Piece>();
+            //Perform discover itself on 3x3 
+            for (int y = Math.Max((int)board.GoalsHeight, (int)playerPawn.Y - 1); y <= Math.Min(playerPawn.X + 1, board.Height-board.GoalsHeight-1); ++x)
+                for (int x = Math.Max(0, (int)playerPawn.X - 1);x <= Math.Min(playerPawn.X+1,board.Width-1); ++x)
+                {
+                    ITaskField currentField = board[(uint)x, (uint)y] as ITaskField;//current field is a task field because of for-loops' bounds
+                    TaskField current = new TaskField
+                    {
+                        x = (uint)x,
+                        y = (uint)y,
+                        timestamp = DateTime.Now,
+                    };
+                    if (currentField.Piece != null) //piece on the board
+                    {
+                        current.pieceId = currentField.Piece.Id;
+                        resultPieces.Add(new Piece
+                        {
+                            id = currentField.Piece.Id,
+                            type = currentField.Piece.Type,
+                            timestamp = DateTime.Now
+                        });
+                    }
+                    if (currentField.PlayerId != null)
+                    {
+                        current.playerId = (ulong)currentField.PlayerId;
+                        if(board.GetPlayer((ulong)currentField.PlayerId).Piece != null) //check for a held piece
+                            resultPieces.Add(new Piece
+                            {
+                                id = board.GetPlayer((ulong)currentField.PlayerId).Piece.Id,
+                                type = board.GetPlayer((ulong)currentField.PlayerId).Piece.Type,
+                                timestamp = DateTime.Now,
+                                playerId = (ulong)currentField.PlayerId
+                            });
+                        
+                    }
+                    //TODO policz dystans do piece
+                    //result.distanceToPiece = ???
+
+                    resultFields.Add(current);
+                }
+            Data result = new Data
+            {
+                playerId = player.id,
+                Pieces = resultPieces.ToArray(),
+                TaskFields = resultFields.ToArray(),
+            };
+            return result;
         }
 
         public Data PerformKnowledgeExchange(KnowledgeExchangeRequest knowledgeExchangeRequest)
@@ -80,5 +130,6 @@ namespace GameMasterCore
         }
 
         private Player GetPlayerForGuid(string guid) => guidToPlayer.FirstOrDefault(pair => pair.Key == guid).Value;
+       
     }
 }
