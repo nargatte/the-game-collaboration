c3e65c4180e672495aed46c1d8039a3a9fadc1da
Ewa Zielińska <zielinskae@student.mini.pw.edu.pl>
2018-03-23 11:13:04 +0100

    refactoring Discover

--- GameMasterCore/GameMaster.cs
+++ GameMasterCore/GameMaster.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Shared;
 using Shared.Components.Boards;
 using Shared.Components.Fields;
+using Shared.Components.Pieces;
 using Shared.Components.Players;
 using Shared.Messages.Communication;
 
@@ -31,43 +32,16 @@ namespace GameMasterCore
             List<TaskField> resultFields = new List<TaskField>();
             List<Piece> resultPieces = new List<Piece>();
             //Perform discover itself on 3x3 
-            for (int y = Math.Max((int)board.GoalsHeight, (int)playerPawn.Y - 1); y <= Math.Min(playerPawn.X + 1, board.Height-board.GoalsHeight-1); ++x)
-                for (int x = Math.Max(0, (int)playerPawn.X - 1);x <= Math.Min(playerPawn.X+1,board.Width-1); ++x)
+            for (int y = Math.Max((int)board.GoalsHeight, (int)playerPawn.Y - 1);
+                y <= Math.Min(playerPawn.Y + 1, board.Height - board.GoalsHeight - 1);
+                ++y)
+                for (int x = Math.Max(0, (int)playerPawn.X - 1);
+                    x <= Math.Min(playerPawn.X + 1, board.Width - 1);
+                    ++x)
                 {
-                    ITaskField currentField = board[(uint)x, (uint)y] as ITaskField;//current field is a task field because of for-loops' bounds
-                    TaskField current = new TaskField
-                    {
-                        x = (uint)x,
-                        y = (uint)y,
-                        timestamp = DateTime.Now,
-                    };
-                    if (currentField.Piece != null) //piece on the board
-                    {
-                        current.pieceId = currentField.Piece.Id;
-                        resultPieces.Add(new Piece
-                        {
-                            id = currentField.Piece.Id,
-                            type = currentField.Piece.Type,
-                            timestamp = DateTime.Now
-                        });
-                    }
-                    if (currentField.PlayerId != null)
-                    {
-                        current.playerId = (ulong)currentField.PlayerId;
-                        if(board.GetPlayer((ulong)currentField.PlayerId).Piece != null) //check for a held piece
-                            resultPieces.Add(new Piece
-                            {
-                                id = board.GetPlayer((ulong)currentField.PlayerId).Piece.Id,
-                                type = board.GetPlayer((ulong)currentField.PlayerId).Piece.Type,
-                                timestamp = DateTime.Now,
-                                playerId = (ulong)currentField.PlayerId
-                            });
-                        
-                    }
-                    //TODO policz dystans do piece
-                    //result.distanceToPiece = ???
-
-                    resultFields.Add(current);
+                    TaskField fieldToReturn = GetFieldInfo(x, y, out Piece[] pieces);
+                    resultFields.Add(fieldToReturn);
+                    resultPieces.AddRange(pieces);
                 }
             Data result = new Data
             {
@@ -86,8 +60,12 @@ namespace GameMasterCore
         public Data PerformMove(Move moveRequest)
         {
             //znajdź gracza po id
+            Player player = GetPlayerForGuid(moveRequest.playerGuid);
+            IPlayer playerPawn = board.GetPlayer(player.id);
             //sprawdź czy może się ruszyć
+
             //rusz
+            //zwróć informacje o obecnym polu i PlayerLocation
             throw new NotImplementedException();
         }
 
@@ -111,18 +89,24 @@ namespace GameMasterCore
         {
             //TODO znajdź playera po id, znajdź jego piece
             Player player = GetPlayerForGuid(testPieceRequest.playerGuid);
-            Piece heldPiece;
-            
+            IPlayer playerPawn = board.GetPlayer(player.id);
+            IPiece heldPiecePawn = playerPawn.Piece;
+            if(heldPiecePawn == null)
+            {
+                return new Data { playerId = player.id }; //player wanted to test inaccessible piece
+            }
+
             //zwróć czy sham
             Data result = new Data
             {
+                playerId = player.id,
                 Pieces = new Piece[] {
                     new Piece
                     {
-                        id = heldPiece.id,
+                        id = heldPiecePawn.Id,
                         timestamp = DateTime.Now,
                         playerId = player.id,
-                        type = heldPiece.type
+                        type = heldPiecePawn.Type
                     }
                 }
             };
@@ -130,6 +114,45 @@ namespace GameMasterCore
         }
 
         private Player GetPlayerForGuid(string guid) => guidToPlayer.FirstOrDefault(pair => pair.Key == guid).Value;
-       
+
+        private TaskField GetFieldInfo(int x, int y, out Piece[] pieces)
+        {
+            List<Piece> piecesToReturn = new List<Piece>();
+            ITaskField currentField = board[(uint)x, (uint)y] as ITaskField;//current field is a task field because of for-loops' bounds
+            TaskField fieldToReturn = new TaskField
+            {
+                x = (uint)x,
+                y = (uint)y,
+                timestamp = DateTime.Now,
+            };
+            if (currentField.Piece != null) //piece on the board
+            {
+                fieldToReturn.pieceId = currentField.Piece.Id;
+                piecesToReturn.Add(new Piece
+                {
+                    id = currentField.Piece.Id,
+                    type = PieceType.unknown,
+                    timestamp = DateTime.Now
+                });
+            }
+            if (currentField.PlayerId != null)
+            {
+                fieldToReturn.playerId = (ulong)currentField.PlayerId;
+                if (board.GetPlayer((ulong)currentField.PlayerId).Piece != null) //check for held piece
+                    piecesToReturn.Add(new Piece
+                    {
+                        id = board.GetPlayer((ulong)currentField.PlayerId).Piece.Id,
+                        type = PieceType.unknown,
+                        timestamp = DateTime.Now,
+                        playerId = (ulong)currentField.PlayerId
+                    });
+
+            }
+            //TODO policz dystans do piece
+            //fieldToReturn.distanceToPiece = ???
+
+            pieces = piecesToReturn.ToArray();
+            return fieldToReturn;
+        }
     }
 }
