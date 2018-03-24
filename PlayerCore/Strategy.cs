aac41e57353096ac6c1efcfb77e86fb077b22fe2
Unknown <krzysiak.grzegorz@gmail.com>
2018-03-24 18:01:29 +0100

    late save

---
+++ PlayerCore/Strategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerCore
+{
+    class Strategy
+    {
+        public void PerformAction(State state)
+        {
+            if(state.)
+        }
+    }
+}
