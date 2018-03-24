5697831c1efb90d6a079fdd06c0c5b5fc1a4f710
Sebastian Majewski <majewskis@student.mini.pw.edu.pl>
2018-03-24 18:02:13 +0100

    tests

---
+++ SharedUnitTests/Components/Boards/BoardTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace SharedUnitTests.Components.Boards
+{
+	[TestFixture]
+	public class BoardTest
+	{
+		[Test]
+		public void Test1()
+		{
+			;
+			Assert.Zero( 0 );
+		}
+	}
+}
