e305b917b33333a560f0eacb333b4fd15d1f0b83
Sebastian Majewski <majewskis@student.mini.pw.edu.pl>
2018-04-05 21:26:10 +0200

    board base tests

--- SharedUnitTests/Components/Boards/BoardBaseTest.cs
+++ SharedUnitTests/Components/Boards/BoardBaseTest.cs
@@ -1,11 +1,11 @@
-﻿//using Moq;
+﻿using Moq;
 using NUnit.Framework;
 using Shared.Components.Boards;
+using Shared.Components.Events;
 using Shared.Components.Factories;
 using Shared.Components.Fields;
 using Shared.Components.Pieces;
 using Shared.Components.Players;
-//using Shared.Enums;
 using System;
 using System.Collections.Generic;
 
@@ -29,35 +29,83 @@ namespace SharedUnitTests.Components.Boards
 			public override void SetPlayer( IPlayer value ) => throw new NotImplementedException();
 			#endregion
 			#region FieldTestType
-			protected BoardBaseTestType( uint width, uint tasksHeight, uint goalsHeight, IBoardPrototypeFactory factory ) : base( width, tasksHeight, goalsHeight, factory )
+			public BoardBaseTestType( uint width, uint tasksHeight, uint goalsHeight, IBoardPrototypeFactory factory ) : base( width, tasksHeight, goalsHeight, factory )
 			{
 			}
+			public void OnFieldChanged() => OnFieldChanged( default, default );
+			public void OnPieceChanged() => OnPieceChanged( default );
+			public void OnPlayerChanged() => OnPlayerChanged( default );
 			#endregion
 		}
+		private static readonly IBoardPrototypeFactory exampleBoardPrototypeFactory;
 		private static readonly object[] constructorParameters;
+		private static readonly object[] constructorParametersWithWidthError;
+		private static readonly object[] constructorParametersWithTasksHeightError;
+		private static readonly object[] constructorParametersWithGoalsHeightError;
 		static BoardBaseTest()
 		{
-			/*constructorParameters = new object[]
+			exampleBoardPrototypeFactory = Mock.Of<IBoardPrototypeFactory>();
+			constructorParameters = new object[]
 			{
-				new object[] { 1u, 3u, 5u,  }
-			};*/
+				new object[] { 1u, 3u, 5u, exampleBoardPrototypeFactory },
+				new object[] { 2u, 4u, 6u, exampleBoardPrototypeFactory }
+			};
+			constructorParametersWithWidthError = new object[]
+			{
+				new object[] { 0u, 1u, 2u, exampleBoardPrototypeFactory },
+			};
+			constructorParametersWithTasksHeightError = new object[]
+			{
+				new object[] { 1u, 0u, 2u, exampleBoardPrototypeFactory },
+			};
+			constructorParametersWithGoalsHeightError = new object[]
+			{
+				new object[] { 1u, 2u, 0u, exampleBoardPrototypeFactory },
+			};
 		}
 		#endregion
 		#region Test
-		/*[TestCaseSource( nameof( constructorParameters ) )]
-		public void ConstructorFillsAllProperties( ulong id, TeamColour team, PlayerType type, DateTime timestamp, IField field, IPlayerPiece piece )
+		[TestCaseSource( nameof( constructorParametersWithWidthError ) )]
+		public void ConstructorThrowsArgumentOutOfRangeExceptionWhenWidthIs0( uint width, uint tasksHeight, uint goalsHeight, IBoardPrototypeFactory factory ) => Assert.That( () =>
 		{
-			var sut = new Player( id, team, type, timestamp, field, piece );
+			var sut = new BoardBaseTestType( width, tasksHeight, goalsHeight, factory );
+		}, Throws.InstanceOf<ArgumentOutOfRangeException>() );
+		[TestCaseSource( nameof( constructorParametersWithTasksHeightError ) )]
+		public void ConstructorThrowsArgumentOutOfRangeExceptionWhenTasksHeightIs0( uint width, uint tasksHeight, uint goalsHeight, IBoardPrototypeFactory factory ) => Assert.That( () =>
+		{
+			var sut = new BoardBaseTestType( width, tasksHeight, goalsHeight, factory );
+		}, Throws.InstanceOf<ArgumentOutOfRangeException>() );
+		[TestCaseSource( nameof( constructorParametersWithGoalsHeightError ) )]
+		public void ConstructorThrowsArgumentOutOfRangeExceptionWhenGoalsHeightIs0( uint width, uint tasksHeight, uint goalsHeight, IBoardPrototypeFactory factory ) => Assert.That( () =>
+		{
+			var sut = new BoardBaseTestType( width, tasksHeight, goalsHeight, factory );
+		}, Throws.InstanceOf<ArgumentOutOfRangeException>() );
+		[TestCase( 1u, 2u, 3u, null )]
+		public void ConstructorThrowsArgumentNullExceptionWhenFactoryIsNull( uint width, uint tasksHeight, uint goalsHeight, IBoardPrototypeFactory factory ) => Assert.That( () =>
+		{
+			var sut = new BoardBaseTestType( width, tasksHeight, goalsHeight, factory );
+		}, Throws.ArgumentNullException );
+		[TestCaseSource( nameof( constructorParameters ) )]
+		public void ConstructorFillsAllProperties( uint width, uint tasksHeight, uint goalsHeight, IBoardPrototypeFactory factory )
+		{
+			var sut = new BoardBaseTestType( width, tasksHeight, goalsHeight, factory );
 			Assert.Multiple( () =>
 			{
-				Assert.AreEqual( id, sut.Id );
-				Assert.AreEqual( team, sut.Team );
-				Assert.AreEqual( type, sut.Type );
-				Assert.AreEqual( timestamp, sut.Timestamp );
-				Assert.AreSame( field, sut.Field );
-				Assert.AreSame( piece, sut.Piece );
+				Assert.AreEqual( width, sut.Width );
+				Assert.AreEqual( tasksHeight, sut.TasksHeight );
+				Assert.AreEqual( goalsHeight, sut.GoalsHeight );
+				Assert.AreSame( factory, sut.Factory );
+				Assert.AreEqual( tasksHeight + 2u * goalsHeight, sut.Height );
 			} );
-		}*/
+		}
+		[TestCaseSource( nameof( constructorParameters ) )]
+		public void EventsAreNotNullByDefault( uint width, uint tasksHeight, uint goalsHeight, IBoardPrototypeFactory factory )
+		{
+			var sut = new BoardBaseTestType( width, tasksHeight, goalsHeight, factory );
+			sut.OnFieldChanged();
+			sut.OnPieceChanged();
+			sut.OnPlayerChanged();
+		}
 		#endregion
 	}
 }
