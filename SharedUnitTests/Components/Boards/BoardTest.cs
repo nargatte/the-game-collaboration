41a8b61f07e19d461c10d77df3f5272bbc6012d8
Sebastian Majewski <majewskis@student.mini.pw.edu.pl>
2018-04-05 22:53:49 +0200

    issue

--- SharedUnitTests/Components/Boards/BoardTest.cs
+++ SharedUnitTests/Components/Boards/BoardTest.cs
@@ -1,12 +1,7 @@
-﻿using Moq;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Shared.Components.Boards;
 using Shared.Components.Factories;
-using Shared.Components.Fields;
-using Shared.Components.Pieces;
-using Shared.Components.Players;
-using Shared.Enums;
-using System;
+using System.Linq;
 
 namespace SharedUnitTests.Components.Boards
 {
@@ -18,10 +13,7 @@ namespace SharedUnitTests.Components.Boards
 		private static readonly object[] constructorParameters;
 		static BoardTest()
 		{
-			var mock = new Mock<IBoardPrototypeFactory>();
-			mock.Setup( factory => factory.GoalField.CreateGoalField( It.IsAny<uint>(), It.IsAny<uint>(), It.IsAny<TeamColour>(), It.IsAny<DateTime>(), It.IsAny<IPlayer>(), It.IsAny<GoalFieldType>() ) ).Returns( ( uint x, uint y, TeamColour team, DateTime timestamp, IPlayer player, GoalFieldType type ) => Mock.Of<IGoalField>( field => field.X == x && field.Y == y && field.Team == team && field.Timestamp == timestamp && field.Player == player && field.Type == type ) );
-			mock.Setup( factory => factory.TaskField.CreateTaskField( It.IsAny<uint>(), It.IsAny<uint>(), It.IsAny<DateTime>(), It.IsAny<IPlayer>(), It.IsAny<int>(), It.IsAny<IFieldPiece>() ) ).Returns( ( uint x, uint y, DateTime timestamp, IPlayer player, int distanceToPiece, IFieldPiece piece ) => Mock.Of<ITaskField>( field => field.X == x && field.Y == y && field.Timestamp == timestamp && field.Player == player && field.DistanceToPiece == distanceToPiece && field.Piece == piece ) );
-			exampleBoardPrototypeFactory = mock.Object;
+			exampleBoardPrototypeFactory = new BoardPrototypeFactory();
 			constructorParameters = new object[]
 			{
 				new object[] { 1u, 3u, 5u, exampleBoardPrototypeFactory },
@@ -35,6 +27,7 @@ namespace SharedUnitTests.Components.Boards
 		{
 			var sut = new Board( width, tasksHeight, goalsHeight, factory );
 			uint height = tasksHeight + 2u * goalsHeight;
+			uint fieldCount = width * height;
 			Assert.Multiple( () =>
 			{
 				Assert.AreEqual( width, sut.Width );
@@ -42,6 +35,9 @@ namespace SharedUnitTests.Components.Boards
 				Assert.AreEqual( goalsHeight, sut.GoalsHeight );
 				Assert.AreSame( factory, sut.Factory );
 				Assert.AreEqual( height, sut.Height );
+				Assert.AreEqual( fieldCount, sut.Fields.Count() );
+				Assert.AreEqual( 0, sut.Pieces.Count() );
+				Assert.AreEqual( 0, sut.Players.Count() );
 			} );
 		}
 		#endregion
