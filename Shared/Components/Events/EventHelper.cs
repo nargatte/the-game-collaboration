fc6d87f4e1dea6f4714966d41b19ff319195eeb4
Sebastian Majewski <majewskis@student.mini.pw.edu.pl>
2018-04-05 20:26:19 +0200

    IReadOnlyBoard + EventHelper

---
+++ Shared/Components/Events/EventHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Shared.Components.Events
+{
+	public static class EventHelper
+	{
+		public static void OnEvent<T>( object sender, EventHandler<T> eventHandler, T eventArgs, [CallerMemberName] string eventName = null )
+		{
+			var exceptionList = new List<Exception>();
+			foreach( EventHandler<T> handler in eventHandler.GetInvocationList() )
+				try
+				{
+					handler( sender, eventArgs );
+				}
+				catch( Exception exception )
+				{
+					exceptionList.Add( exception );
+				}
+			if( exceptionList.Count > 0 )
+				throw new AggregateException( $"Exceptions thrown by event subscribers called in { eventName }.", exceptionList );
+		}
+	}
+}
