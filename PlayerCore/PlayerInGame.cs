11e458dfc0fc0c5c93ec6e1f3336a8199a67e628
Grzegorz Krzysiak <krzysiak.grzegorz@gmail.com>
2018-04-06 00:16:04 +0200

    change names

---
+++ PlayerCore/PlayerInGame.cs
@@ -0,0 +1,28 @@
+﻿using GameMasterCore;
+using Shared.Messages.Communication;
+
+namespace PlayerCore
+{
+    class PlayerInGame
+    {
+        private IGameMaster GameMaster { get; }
+
+        private Strategy Strategy { get; }
+
+        private State State { get; }
+
+        public PlayerInGame(IGameMaster gameMaster, Game game, uint playerId, string playerGuid, uint gameId)
+        {
+            this.GameMaster = gameMaster;
+            State = new State(game, playerId, gameId, playerGuid);
+            Strategy = new Strategy(gameMaster, State);
+        }
+
+        public void PerformAction()
+        {
+            Strategy.PerformAction();
+        }
+
+
+    }
+}
