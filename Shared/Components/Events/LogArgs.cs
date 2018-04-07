40e8c307e36341cfd64ef2538adc455bff3c685a
Sebastian Majewski <majewskis@student.mini.pw.edu.pl>
2018-04-07 07:28:02 +0200

    added LogArgs

---
+++ Shared/Components/Events/LogArgs.cs
@@ -0,0 +1,26 @@
+﻿using Shared.Enums;
+using System;
+
+namespace Shared.Components.Events
+{
+	class LogArgs : EventArgs
+	{
+		string Type { get; }
+		DateTime Timestamp { get; }
+		ulong GameId { get; }
+		ulong PlayerId { get; }
+		string PlayerGuid { get; }
+		TeamColour Colour { get; }
+		PlayerType Role { get; }
+		public LogArgs( string type, DateTime timestamp, ulong gameId, ulong playerId, string playerGuid, TeamColour colour, PlayerType role )
+		{
+			Type = type;
+			Timestamp = timestamp;
+			GameId = gameId;
+			PlayerId = playerId;
+			PlayerGuid = playerGuid;
+			Colour = colour;
+			Role = role;
+		}
+	}
+}
