d22d4546d8fed546bce9438e2ffd5e8a4befdaaa
Sebastian Majewski <majewskis@student.mini.pw.edu.pl>
2018-04-08 03:44:05 +0200

    empty

---
+++ SingleGame/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using SingleGame.ViewModels.Commands;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using IModel = SingleGame.Models.IMainM;
+
+namespace SingleGame.ViewModels.Base
+{
+	interface IViewModel : INotifyPropertyChanged
+	{
+		IModel Model { get; }
+		IAsyncCommand InitializeAsyncCommand { get; }
+	}
+	class ViewModel : IViewModel
+	{
+		#region IViewModel
+		public virtual event PropertyChangedEventHandler PropertyChanged = delegate { };
+		public virtual IModel Model { get; private set; }
+		public virtual IAsyncCommand InitializeAsyncCommand { get; protected set; }
+		#endregion
+		#region ViewModel
+		protected virtual Task InitializeAsync() => Task.CompletedTask;
+		private ViewModel() => InitializeAsyncCommand = new AsyncCommand( InitializeAsync );
+		public ViewModel( IModel model = null ) : this() => Model = model;
+		protected bool SetProperty<T>( ref T backingField, T value, [CallerMemberName] string propertyName = null )
+		{
+			if( EqualityComparer<T>.Default.Equals( backingField, value ) )
+				return false;
+			else
+			{
+				backingField = value;
+				OnPropertyChanged( propertyName );
+				return true;
+			}
+		}
+		protected void OnPropertyChanged( [CallerMemberName] string propertyName = null ) => PropertyChanged.Invoke( this, new PropertyChangedEventArgs( propertyName ) );
+		#endregion
+	}
+}
---
+++ SingleGame/ViewModels/BoardVM.cs
@@ -0,0 +1,15 @@
+﻿using SingleGame.ViewModels.Base;
+
+namespace SingleGame.ViewModels
+{
+	interface IBoardVM : IViewModel
+	{
+	}
+	class BoardVM : ViewModel, IBoardVM
+	{
+		#region IBoardVM
+		#endregion
+		#region BoardVM
+		#endregion
+	}
+}
