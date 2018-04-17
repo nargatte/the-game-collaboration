5a5df506c975ab850cb776b671f021a7ab25deaf
Marta <martarut1994@gmail.com>
2018-04-17 18:16:33 +0200

    IStrategy intefrace added

---
+++ Shared/Interfaces/IStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Messages.Communication;
+
+namespace Shared.Interfaces
+{
+    public interface IStrategy
+    {
+        Data PerformAction();
+        IGameMaster GameMaster { get; set; } 
+        
+    }
+}
