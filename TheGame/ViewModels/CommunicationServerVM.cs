d273316953c37a83b0db11ce1c054b48760e07af
Sebastian Majewski <majewskis@student.mini.pw.edu.pl>
2018-06-08 06:01:14 +0200

    blind

--- TheGame/ViewModels/CommunicationServerVM.cs
+++ TheGame/ViewModels/CommunicationServerVM.cs
@@ -1,19 +1,86 @@
-﻿using TheGame.ViewModels.Base;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CommunicationServerCore.Interfaces.Modules;
+using Shared.Components.Exceptions;
+using Shared.Interfaces.Tasks;
+using TheGame.ViewModels.Base;
 
 namespace TheGame.ViewModels
 {
-	interface ICommunicationServerVM : IViewModel
+	interface ICommunicationServerVM : IViewModel, IRunnable
 	{
+		int Id { get; }
+		string Header { get; }
+		ICommunicationServerModule Module { get; }
+		string Error { get; }
 	}
 	class CommunicationServerVM : ViewModel, ICommunicationServerVM
 	{
 		#region ViewModel
 		#endregion
 		#region ICommunicationServerVM
+		public virtual async Task RunAsync( CancellationToken cancellationToken )
+		{
+			try
+			{
+				using( var cts = CancellationTokenSource.CreateLinkedTokenSource( cancellationToken ) )
+				{
+					cancellationTokenSource = cts;
+					cts.Token.ThrowIfCancellationRequested();
+					var task = Task.Run( async () => await Module.RunAsync( cts.Token ).ConfigureAwait( false ) );
+					try
+					{
+						await task;
+					}
+					catch( OperationCanceledException )
+					{
+					}
+					finally
+					{
+						if( task.IsFaulted )
+						{
+							Error += "Module faulted:\n";
+							foreach( var e in task.Exception.Flatten().InnerExceptions )
+								if( e is DisconnectionException )
+									Error += $"Disconnection ( { e.Message } ).\n";
+								else
+									Error += $"{ e.ToString() }\n";
+						}
+						else if( task.IsCanceled )
+							Error += "Module canceled.";
+						else
+							Error += "Module completed.";
+					}
+				}
+			}
+			catch( Exception e )
+			{
+				Error += $"{ e.ToString() }\n";
+			}
+		}
+		public virtual int Id { get; }
+		private string header;
+		public virtual string Header
+		{
+			get => header;
+			protected set => SetProperty( ref header, value );
+		}
+		public virtual ICommunicationServerModule Module { get; }
+		private string error;
+		public virtual string Error
+		{
+			get => error;
+			protected set => SetProperty( ref error, value );
+		}
 		#endregion
 		#region CommunicationServerVM
-		public CommunicationServerVM()
+		private CancellationTokenSource cancellationTokenSource;
+		public CommunicationServerVM( int id, ICommunicationServerModule module )
 		{
+			Id = id;
+			Header = $"Player #{ id } - anonymous";
+			Module = module;
 		}
 		#endregion
 	}
--- TheGame/ViewModels/MainVM.cs
+++ TheGame/ViewModels/MainVM.cs
@@ -1,19 +1,91 @@
-﻿using TheGame.ViewModels.Base;
+﻿using CommunicationServerCore.Components.Factories;
+using CommunicationServerCore.Components.Modules;
+using CommunicationServerCore.Components.Options;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using TheGame.ViewModels.Base;
+using TheGame.ViewModels.Commands;
 
 namespace TheGame.ViewModels
 {
 	interface IMainVM : IViewModel
 	{
+		IAsyncCommand KillAsyncCommand { get; }
+		ObservableCollection<ICommunicationServerVM> CommunicationServers { get; }
+		string CommunicationServerPort { get; set; }
+		string CommunicationServerConf { get; set; }
+		IAsyncCommand RunCommunicationServerAsyncCommand { get; }
 	}
 	class MainVM : ViewModel, IMainVM
 	{
 		#region ViewModel
 		#endregion
 		#region IMainVM
+		public virtual IAsyncCommand KillAsyncCommand { get; }
+		public virtual ObservableCollection<ICommunicationServerVM> CommunicationServers { get; } = new ObservableCollection<ICommunicationServerVM>();
+		private string communicationServerPort;
+		public virtual string CommunicationServerPort
+		{
+			get => communicationServerPort;
+			set => SetProperty( ref communicationServerPort, value );
+		}
+		private string communicationServerConf;
+		public virtual string CommunicationServerConf
+		{
+			get => communicationServerConf;
+			set => SetProperty( ref communicationServerConf, value );
+		}
+		public virtual IAsyncCommand RunCommunicationServerAsyncCommand { get; }
 		#endregion
 		#region MainVM
+		private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+		private int communicationServerId = 0;
+		private int gameMasterId = 0;
+		private int playerId = 0;
 		public MainVM()
 		{
+			KillAsyncCommand = new AsyncCommand( Kill );
+			RunCommunicationServerAsyncCommand = new AsyncCommand( RunCommunicationServer );
+		}
+		protected Task Kill()
+		{
+			try
+			{
+				try
+				{
+					cancellationTokenSource.Cancel();
+				}
+				finally
+				{
+					cancellationTokenSource.Dispose();
+				}
+			}
+			catch( Exception )
+			{
+			}
+			return Task.CompletedTask;
+		}
+		protected async Task RunCommunicationServer()
+		{
+			try
+			{
+				string[] args = new string[]
+				{
+					"--port", CommunicationServerPort, "--conf", CommunicationServerConf
+				};
+				var options = new CommunicationServerOptions( args );
+				var module = new CommunicationServerModule( "localhost", options.Port, options.Conf, new CommunicationServerFactory() );
+				var vm = new CommunicationServerVM( communicationServerId++, module );
+				CommunicationServers.Add( vm );
+				await vm.RunAsync( cancellationTokenSource.Token ).ConfigureAwait( false );
+			}
+			catch( Exception e )
+			{
+				MessageBox.Show( e.ToString(), "Exception" );
+			}
 		}
 		#endregion
 	}
